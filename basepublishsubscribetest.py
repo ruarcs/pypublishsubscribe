8632455442be6b1403b03687619e8523d4090d1e
Ruarc Sorensen <ruarcs@gmail.com>
2015-05-08 15:08:59 +0100

    Update some comments on the base test class.

--- basepublishsubscribetest.py
+++ basepublishsubscribetest.py
@@ -7,24 +7,25 @@ from threading import Thread
 from Queue import Queue
 
 class BasePublishSubscribeTest(unittest.TestCase):
-    """A set of tests to ensure that the Publish-Subscribe
-        server obeys the contract provided in the exercise
+    """A skeleton for a test that allows requests to
+        be sent to the server, and clears the server
+        after each test, to allow test independence.
         """
     port = 8081
     publisher = PublishSubscribeResource()
-    
+
     @classmethod
     def setUpClass(cls):
         #Start the server
         site = server.Site(cls.publisher)
         reactor.listenTCP(cls.port, site)
         Thread(target=reactor.run, args=(False,)).start()
-    
-    @classmethod  
+
+    @classmethod
     def tearDownClass(cls):
         # Stop the server
         reactor.callFromThread(reactor.stop)
-        
+
     def tearDown(self):
         # After each individual test clear the server's data
         # structures to give test independence.
